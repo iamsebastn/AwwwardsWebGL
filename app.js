36c8a5a483359155a158f572de6acbeb70d145b2
Sebastian Wolf <sebastian.wolf298@gmail.com>
2022-12-14 14:59:17 +0100

    Create zoom effect

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ import * as THREE from 'three';
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls.js";
 import fragment from "./shaders/fragment.glsl";
 import vertex from "./shaders/vertex.glsl";
-import testTexture from "./water.jpg";
+import testTexture from "./texture.jpg";
+import * as dat from "dat.gui"
 
 export default class Sketch{
     constructor(options){
@@ -10,8 +11,10 @@ export default class Sketch{
         this.width=this.container.offsetWidth;
         this.height=this.container.offsetHeight;
 
-        this.camera = new THREE.PerspectiveCamera( 70, this.width/this.height, 0.01, 10 );
-        this.camera.position.z = 1;
+        this.camera = new THREE.PerspectiveCamera( 30, this.width/this.height, 10, 1000 );
+        this.camera.position.z = 600;
+
+        this.camera.fov = 2*Math.atan((this.height/2)/600) * 180/Math.PI
 
         this.scene = new THREE.Scene();
 
@@ -27,12 +30,21 @@ export default class Sketch{
         
         
         this.time = 0;
+        this.setupSettings()
         this.resize()
         this.addObjects()
         this.render();
         
         this.setupResize()
 	}
+
+    setupSettings(){
+        this.settings = {
+            progress: 0
+        }
+        this.gui = new dat.GUI();
+        this.gui.add(this.settings,"progress",0,1,0.001);
+    }
 
 	resize(){
 		this.width = this.container.offsetWidth;
@@ -48,27 +60,30 @@ export default class Sketch{
 	}
 
 	addObjects() {
-		this.geometry = new THREE.PlaneBufferGeometry( 0.5, 0.5, 100, 100);
-        // this.geometry = new THREE.SphereBufferGeometry( 0.5, 160,160);
+		this.geometry = new THREE.PlaneBufferGeometry( 350, 350, 100, 100);
         console.log(this.geometry)
         this.material = new THREE.ShaderMaterial({
             // wireframe: true,
             uniforms: {
                 time: { value: 1.0 },
+                uProgress: { value: 1.0 },
                 uTexture: {value: new THREE.TextureLoader().load(testTexture)},
-                resolution: { value: new THREE.Vector2() }
+                uResolution: { value: new THREE.Vector2(this.width, this.height)},
+                uQuadSize: { value: new THREE.Vector2(300, 300)}
             },
             vertexShader: vertex,
             fragmentShader: fragment,
         })
-
 		this.mesh = new THREE.Mesh( this.geometry, this.material );
 		this.scene.add( this.mesh );
+        this.mesh.position.x = 300;
+        this.mesh.rotation.z = 0.5;
 	}
 
 	render(){
-        this.time += 0.03;
-        this.material.uniforms.time.value = this.time
+        this.time += 0.05;
+        this.material.uniforms.time.value = this.time;
+        this.material.uniforms.uProgress.value = this.settings.progress;
         this.mesh.rotation.x = this.time / 2000;
         this.mesh.rotation.y = this.time / 1000;
 
--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -1,11 +1,17 @@
 uniform float time;
-varying float pulse;
+uniform float uProgess;
+uniform vec2 uResolution;
+uniform vec2 uQuadSize;
 
 varying vec2 vUv;
 void main() {
     vUv = uv;
-    vec3 newPosition = position;
-    newPosition.z = 0.0*sin(length(position)*30.);
-    pulse = 20.*newPosition.z;
-    gl_Position = projectionMatrix * modelViewMatrix * vec4( newPosition, 1.0 );
+    vec4 defaultState = modelMatrix*vec4(position, 1.0);
+    vec4 fullScreenState = vec4(position, 1.0);
+    fullScreenState.x *= uResolution.x/uQuadSize.x;
+    fullScreenState.y *= uResolution.y/uQuadSize.y;
+
+    vec4 finalState = mix(defaultState, fullScreenState, uProgess);
+
+    gl_Position = projectionMatrix * viewMatrix * fullScreenState;
 }
